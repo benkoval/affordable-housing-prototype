267f885a659d5b03af450fc73c82afdbd46cbc62
bennykoval <bethanykoval@gmail.com>
2019-09-03 17:00:24 -0400

    UI

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        <nav class='navBar'>
+            <ul>
+                <li><img class='logo' src='https://codefordurham.com/wp-content/themes/code-for-durham-website-master/images/logo/logo_2X.png' alt='Code for Durham logo'/></li>
+                <li class='navOptions'><a href='#lorem'>Lorem</a></li>
+                <li class='navOptions'><a href='#ipsum'>Ipsum</a></li>
+                <li class='navOptions'><a href='#dolor'>Dolor</a></li>
+                <li class='navOptions'><a href='#sit'>Sit</a></li>
+                <li class='navOptions'><a href='#amet'>Amet</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
---
+++ src/components/Main.vue
@@ -0,0 +1,37 @@
+<template>
+    <main class='mainContainer'>
+        <section class='textContainer' id='lorem'>
+            <h2 class='title'>Lorem</h2>
+
+            <p class='text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid cumque non quaerat vitae animi? Assumenda nostrum alias corporis fugiat facilis vero ducimus, beatae aliquam, in perferendis, consectetur saepe unde velit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid cumque non quaerat vitae animi? Assumenda nostrum alias corporis fugiat facilis vero ducimus, beatae aliquam, in perferendis, consectetur saepe unde velit.</p>
+        </section>
+
+        <section class='textContainer' id='ipsum'>
+            <h2 class='title'>Ipsum</h2>
+
+            <p class='text'>Fixie dirty durham consensus big green wall beaver queen sustainable urban ministries dapper, broad street wool e bull old north innovation bowtie maker, southsquare historic preservation maker full frame sodu the buehler. The bulls hipster blues major sodu duke chapel dino trail eagles, major the state of things ninth street miami boulevard diy. Durham goldenbelt duke park local erwin road saint joseph's, start-up adf miami boulevard bulltown strutters ccb plaza french press, erwin terrace the double nickel merge records elf. Angier drive medicine dance the indy one forty seven train arm wrestling, duke chapel university science and math partner erwin square black wall street major, seventy french press dance dur'm 15-501. Blue devils bouncing bulldogs third fridays the double nickel chapel drive beer doughman seventy west village wunc, northeast central luewwd hipster hub coworking erwin square authentic innovation. Seeds the buehler oprah building walltown arm wrestling historic preservation local, blackwell urban ministries old north the kress historic preservation doughman, trinity park foodie bay 7 eno river bragtown.</p>
+        </section>
+
+        <section class='textContainer' id='dolor'>
+            <h2 class='title'>Dolor</h2>
+            
+            <div class='halfContainer'>
+                <p class='halfText'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid cumque non quaerat vitae animi? Assumenda nostrum alias corporis fugiat facilis vero ducimus, beatae aliquam, in perferendis, consectetur saepe unde velit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid cumque non quaerat vitae animi? Assumenda nostrum alias corporis fugiat facilis vero ducimus, beatae aliquam, in perferendis, consectetur saepe unde velit.</p>
+                <img src='http://placekitten.com/600/800' alt-text='Place kitten' class='placeKitty'/>
+            </div>
+        </section>
+
+        <section class='textContainer' id='sit'>
+            <h2 class='title'>Sit</h2>
+
+            <p class='text'>Fixie dirty durham consensus big green wall beaver queen sustainable urban ministries dapper, broad street wool e bull old north innovation bowtie maker, southsquare historic preservation maker full frame sodu the buehler. The bulls hipster blues major sodu duke chapel dino trail eagles, major the state of things ninth street miami boulevard diy. Durham goldenbelt duke park local erwin road saint joseph's, start-up adf miami boulevard bulltown strutters ccb plaza french press, erwin terrace the double nickel merge records elf. Angier drive medicine dance the indy one forty seven train arm wrestling, duke chapel university science and math partner erwin square black wall street major, seventy french press dance dur'm 15-501. Blue devils bouncing bulldogs third fridays the double nickel chapel drive beer doughman seventy west village wunc, northeast central luewwd hipster hub coworking erwin square authentic innovation. Seeds the buehler oprah building walltown arm wrestling historic preservation local, blackwell urban ministries old north the kress historic preservation doughman, trinity park foodie bay 7 eno river bragtown.</p>
+        </section>
+
+        <section class='textContainer' id='amet'>
+            <h2 class='title'>Amet</h2>
+
+            <p class='text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid cumque non quaerat vitae animi? Assumenda nostrum alias corporis fugiat facilis vero ducimus, beatae aliquam, in perferendis, consectetur saepe unde velit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid cumque non quaerat vitae animi? Assumenda nostrum alias corporis fugiat facilis vero ducimus, beatae aliquam, in perferendis, consectetur saepe unde velit.</p>
+        </section>
+
+    </main>
+</template>
